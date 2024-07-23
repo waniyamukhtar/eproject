bc16b508252efa1df06a5b35f4b46defa1ce3b0c
waniyamukhtar <waniyamukhtar91@gmail.com>
2024-07-23 15:24:10 +0500

    project

---
+++ dashboard1/config.php
@@ -0,0 +1,16 @@
+
+<?php
+
+
+
+$con=mysqli_connect("localhost","root","","dashboard1");
+
+
+if($con)
+{
+	echo "good";
+}
+
+
+?>
+
---
+++ hospital/logout.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+
+// session_unset ($_SESSION['un']);
+
+
+session_destroy();
+
+
+?>
+
+
